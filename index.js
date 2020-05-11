0b62a950045f962d7cd6b04f4920ee5a0f22463a
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-11 20:55:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(arr) {
+  let w = arr.find( r => r.result === "W" )
+	  if (w === undefined)
+      return undefined
+    else
+      return w.year
 }
