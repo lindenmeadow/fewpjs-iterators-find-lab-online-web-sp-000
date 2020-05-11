1dd69de48e967fbcc7f2d3ea100377fcd6bdef32
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-11 20:54:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const testVar = {}
 
 function superbowlWin(arr) {
-  let win = array.find( r => r.result === "W" )
+  let win = arr.find( r => r.result === "W" )
 	  if (win === undefined)
       return undefined
     else
